79db064e5ea73b1bc18921cab8bd0a9fa0a887f9
Tony Choi <tonym.3553@gmail.com>
2020-01-14 19:27:46 -0800

    Chapter 6 Exercise 1

--- chapter-06-exercises/exercise-1/exercise.R
+++ chapter-06-exercises/exercise-1/exercise.R
@@ -1,7 +1,7 @@
 # Exercise 1: calling built-in functions
 
 # Create a variable `my_name` that contains your name
-
+my_name <- "Tony Choi"
 
 # Create a variable `name_length` that holds how many letters (including spaces)
 # are in your name (use the `nchar()` function)
