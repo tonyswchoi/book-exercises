2ee671eda21b2de105b7a650b4c0285821af040b
Tony Choi <tony@Tonys-MacBook-Pro.local>
2020-01-30 23:45:01 -0800

    Week 4 Exercises

--- chapter-11-exercises/exercise-1/exercise.R
+++ chapter-11-exercises/exercise-1/exercise.R
@@ -11,18 +11,18 @@ library(fueleconomy)
 
 # You should now have access to the `vehicles` data frame
 # You can use `View()` to inspect it
-
+View(vehicles)
 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
-
+manufacturers <- vehicles$make
 
 # Use the `unique()` function to determine how many different car manufacturers
 # are represented by the data set
-
+length(unique(manufacturers))
 
 # Filter the data set for vehicles manufactured in 1997
-
+vehicles_1997 <- vehicles[vehicles$year == 1997, ]
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
 # Hint: use the `order()` function to get a vector of indices in order by value
@@ -32,16 +32,18 @@ library(fueleconomy)
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
-
+vehicles_1997$average <- (vehicles_1997$hwy + vehicles_1997$cty) / 2
 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
 # Save this new data frame in a variable.
-
+vehicles_2wd <- vehicles[vehicles$drive == "2-Wheel Drive", ]
+efficient_2wd <- vehicles_2wd[vehicles_2wd$cty > 20, ]
 
 # Of the above vehicles, what is the vehicle ID of the vehicle with the worst 
 # hwy mpg?
 # Hint: filter for the worst vehicle, then select its ID.
+efficient_2wd[efficient_2wd$hwy == min(efficient_2wd$hwy), "id"]
 
 
 # Write a function that takes a `year_choice` and a `make_choice` as parameters, 
